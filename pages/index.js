373858cc600a2e8ab6c73a69b54517ec22ee9db5
Bruno Abacherli <38927493+BrunoAbacherli@users.noreply.github.com>
2023-09-02 21:32:37 +0000

    nova frase adicionada como um teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return <h1> Mais uma tarefa concluída! 😎</h1>
+    return <h1> Mais uma tarefa concluída! 😎</h1>;
+    return <p> Uma nova forma de começar coisas novas por aqui! </p>
 }
 
 export default Home;
